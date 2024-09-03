9ffada30924abe58952c8bd6b80d9fcec5254a13
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2024-09-03 13:57:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🔍 Tu estrategia de caza")
+st.write("""¡Encuentra la mejor forma de cazar en Castilla y La Mancha!""")
+st.markdown("""---""")
+
+# DIMENSION 1: Ubicación
+st.subheader("💼 Paso 1: Ubicación")
+st.write('**¿En qué provincia quieres cazar?**')
+provincias = 'BURGOS','SEGOVIA','SORIA','AVILA','LEON','VALLADOLID','PALENCIA','ZAMORA','SALAMANCA']
+D1 = st.multiselect("", provincias, provincias)
