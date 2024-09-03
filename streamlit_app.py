7cdc1f0d6a60b91d17ecfad6c6de2dc44f1d544f
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2024-09-03 15:58:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statsmodels
 import seaborn as sns
-sns.set(style='dark')
 from statsmodels.tsa.arima.model import ARIMA
 import warnings
 
@@ -64,7 +63,7 @@ def predecir_caza(provincias, animales):
             })
       
       # Visualization
-      #plt.style.use('seaborn-darkgrid')  # Set style
+      plt.style.use('dark_background')  # Set style
       fig, ax = plt.subplots(figsize=(12, 8))
       ax.plot(animal_provincia.TEMPORADA, capturas, label='Histórico', marker='o', linestyle='-', color='b', markersize=8)
       temporadas_ext = list(animal_provincia.TEMPORADA) + ['2023-2024']
