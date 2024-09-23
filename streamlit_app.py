2bc48450c8c4373a16c5d0a3c0834acc87560dec
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2024-09-23 20:44:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,7 +158,7 @@ if seleccion == "Inicio":
         return None
     # If there are results:
     else: 
-        st.write("⭐ Para maximizar tu probabilidad de éxito, te recomendamos que cases...")
+        st.write("⭐ Para maximizar tu probabilidad de éxito, te recomendamos que caces...")
         resultados = pd.DataFrame(resultados)
         resultados[["Previsión de caza mínima", "Previsión de caza media", "Previsión de caza máxima"]] = resultados[["Previsión de caza mínima", "Previsión de caza media", "Previsión de caza máxima"]].astype(int)
         resultados = resultados.sort_values(by=['Previsión de caza media']
