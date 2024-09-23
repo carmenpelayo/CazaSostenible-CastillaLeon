c791743e96549e5e03195e5d8136c153a2939195
Carmen Pelayo Fernández <90655687+carmenpelayo@users.noreply.github.com>
2024-09-23 23:48:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,14 +178,14 @@ if seleccion == "🎯 ¡Traza tu estrategia de caza sostenible!":
       st.markdown("""---""")
       st.subheader("🏆 ¡Tus resultados!")
       match = predecir_caza(opcion1, opcion2)
-      st.write("Siguiendo una estrategia de caza sostenible, estás contribuyendo al Objetivo 15 de los Objetivos de Desarrollo Sostenible de las Naciones Unidas.")
-      st.image("images/ODS.png")
+      st.write("Siguiendo una estrategia de caza sostenible, estás contribuyendo al Objetivo 15 (*Vida de Ecosistemas Terrestres*) de los **Objetivos de Desarrollo Sostenible de las Naciones Unidas**.")
+      st.image("images/ODS.png", width=175)
       
 # Sección de Caza Responsable
 if seleccion == "🎓 ¡Aprende más sobre la caza sostenible!":
     st.title("Aprende más sobre la caza sostenible")
     st.write("La caza sostenible es esencial para la **preservación de los ecosistemas** y el **mantenimiento de las poblaciones de fauna silvestre**. Estos objetivos están en línea con el Objetivo 15 de los Objetivos de Desarrollo Sostenible de las Naciones Unidas.")
-    st.image("images/ODS.png", width=300)
+    st.image("images/ODS.png", width=175)
     
     # Buenas prácticas y consejos
     st.subheader("✏️ Buenas prácticas")
